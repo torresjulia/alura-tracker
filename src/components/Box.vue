0b35fc43a2ca759e46cb200e9df3a0ca53b5d424
Julia Torres <juliadittoro@yahoo.com>
2023-07-11 12:12:12 -0300

    First commit

---
+++ src/components/Box.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="box has-text-weight-bold" :style="estilos">
+        <slot></slot>
+
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent }  from 'vue'
+
+export default defineComponent({
+    name: 'Box',
+    data () {
+        return {
+            estilos: { //outra opção para estilização, via JS. *:style="estilos"* 
+                background: '#FAF0CA'
+            }
+        }
+    }
+});
+</script>
---
+++ src/components/Cronometro.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="dis´play">
+        <strong> {{ tempoDecorrido }}</strong>
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'Cronometro',
+    props: {
+        tempoEmSegundos: {
+            type: Number, 
+            default: 0
+        }
+    },
+    computed: {
+        tempoDecorrido(): string {
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substring(11, 8)
+        }
+    },
+})
+</script>
+<style scoped>
+.display {
+    color: var(--texto-primario);
+}
+</style>
